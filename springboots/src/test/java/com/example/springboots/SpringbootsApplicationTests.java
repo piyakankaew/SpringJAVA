86a5a99c8232e919e9a3ccc3124f7d7feee84224
piyakankaew <45116403+piyakankaew@users.noreply.github.com>
2020-09-25 12:53:00 +0700

    Add files via upload

---
+++ springboots/src/test/java/com/example/springboots/SpringbootsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springboots;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
