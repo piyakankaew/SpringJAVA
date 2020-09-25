db8c056a7486f69c1f7cb5c487956332560a75fe
piyakankaew <45116403+piyakankaew@users.noreply.github.com>
2020-09-25 12:51:26 +0700

    Add files via upload

---
+++ springvscode/src/main/java/com/example/springvscode/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.example.springvscode;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringvscodeApplication.class);
+	}
+
+}
---
+++ springvscode/src/test/java/com/example/springvscode/SpringvscodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springvscode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringvscodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
